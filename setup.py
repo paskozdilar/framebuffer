933b96f69fec0ba074c19849a72bcd35c1862a86
paskozdilar <pasko.zdilar@checklens.ai>
2021-04-13 15:25:44 +0200

    Bump to version 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as requirements:
 
 setuptools.setup(
     name="framebuffer",
-    version="1.0.0",
+    version="1.0.1",
     author="Pasko Zdilar",
     author_email="paskozdilar@gmail.com",
     description="SharedMemory-based frame buffer client/server library",
